62ff457c4e73f188413ef4ac7c4d7244928955b4
Nishil Shah <nishil94@gmail.com>
2014-11-19 15:19:51 -0600

    Generator is done but not tested
    generator is completed but not tested. should be able to generate
    passwords of n length.

--- passwords/Generator.java
+++ passwords/Generator.java
@@ -2,22 +2,24 @@ package passwords;
 
 public class Generator {
 	private int[] begin_frequency;
+	//assume row values are the first letter and the column values are the second letter
 	private int[][] diagram_frequency;
-	private long total_diagrams;
+	//contains the total for each row
+	private long[] total_diagrams;
 	private long total_beginning;
 	
 	public Generator()
 	{
 		begin_frequency = new int[26];
 		diagram_frequency = new int[26][26];
-		total_diagrams = 0;
+		total_diagrams = new long[26];
 		total_beginning = 0;
 	}
 	
 	public void update_diagram(int row, int col)
 	{
 		diagram_frequency[row][col]+=1;
-		total_diagrams++;
+		total_diagrams[row]++;
 	}
 	
 	public void update_begin(int row)
@@ -29,18 +31,40 @@ public class Generator {
 	public String createPassword(int length)
 	{
 		String password = "";
-		
+		char char_index;
+
 		int beg_letter = (int)(Math.random() * total_beginning);
-		password+=getBegLetter(beg_letter);
+		char_index = getBegLetter(beg_letter);
+		password+= ""+char_index;
 		
 		for(int k = 1; k < length; k++)
 		{
-			//password+=getDiagramLetter();
+			int second_letter = (int)(Math.random() * total_diagrams[char_index-97]);
+			char_index = getDiagramLetter(char_index-97, second_letter);
+			password+= ""+char_index;			
 		}
 		return password;
 	}
-	
-	private String getBegLetter(int chosen)
+	private char getDiagramLetter(int index, int chosen)
+	{
+		int base = 0;
+		int max = 0;
+		int letter_rep = 0;
+		
+		for(int col = 0; col < diagram_frequency[index].length; col++)
+		{
+			base = max;
+			max += diagram_frequency[index][col];
+			if(chosen >= base && chosen <= max)
+			{
+				letter_rep = col;
+				break;
+			}
+		}
+		letter_rep+=97;
+		return (char)letter_rep;
+	}
+	private char getBegLetter(int chosen)
 	{
 		int base = 0;
 		int max = 0;
@@ -60,6 +84,6 @@ public class Generator {
 			}
 		}
 		letter_rep+=97; //get ascii code
-		return ""+(char)letter_rep; 
+		return (char)letter_rep; 
 	}
 }
